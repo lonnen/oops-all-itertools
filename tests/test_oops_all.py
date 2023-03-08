dd216c96a252220c902db9cbc375823b0993750a
lonnen <chris.lonnen@gmail.com>
2023-03-08 02:00:03 -0800

    add a single test
    this is a quick 'n dirty check to make sure things were imported

---
+++ tests/test_oops_all.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+import oops_all_itertools as oai
+
+
+class DoesItImport(TestCase):
+
+    def test_quantity(self):
+        """Test that a specific pile of stuff was imported"""
+        self.assertEqual(len(dir(oai)), 167)
